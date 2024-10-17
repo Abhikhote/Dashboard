f28b8d7cc2df8e2e890790a6994c7db86b143ca6
Abhikhote <abhijitkhote@gmail.com>
2024-10-17 21:29:32 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ContextProvider } from './contexts/ContextProvider'
+
+createRoot(document.getElementById('root')).render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+
+
+)
